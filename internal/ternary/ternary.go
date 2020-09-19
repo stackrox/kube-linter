80ef6cbdcd014f68722ef47486817e5ca31b4313
Viswajith Venugopal <viswajithiii@gmail.com>
2020-09-18 18:07:37 -0700

    PR comments

--- internal/ternary/ternary.go
+++
@@ -1,9 +0,0 @@
-package ternary
-
-// String does a ternary statement on strings.
-func String(condition bool, ifTrue, ifFalse string) string {
-	if condition {
-		return ifTrue
-	}
-	return ifFalse
-}
