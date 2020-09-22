f959f187d4ee663db7adfe01c34f42b1c66aa6ae
Viswajith Venugopal <viswajithiii@gmail.com>
2020-09-22 12:36:21 -0700

    PR comments

--- scripts/create_github_release.sh
+++ scripts/create_github_release.sh
@@ -11,14 +11,15 @@ tag="$1"
 bin_dir="$2"
 [[ -n "${tag}" && -n "${bin_dir}" ]] || die "Usage: $0 <tag> <binary_directory>"
 
-mkdir /tmp/release-artifacts
+tmp_dir="$(mktemp -d)"
 for os in darwin linux windows; do
   bin_name="kube-linter"
   if [[ "${os}" == "windows" ]]; then
     bin_name="kube-linter.exe"
   fi
-  tar -C "${bin_dir}/${os}" -czf "/tmp/release-artifacts/kube-linter-${os}.tar.gz" "${bin_name}"
+  tar -C "${bin_dir}/${os}" -czf "${tmp_dir}/kube-linter-${os}.tar.gz" "${bin_name}"
+  zip "${tmp_dir}/kube-linter-${os}.zip" "${bin_dir}/${os}/${bin_name}"
 done
 
-ghr -prerelease -n "v${tag}" "${tag}" /tmp/release-artifacts
-rm -r /tmp/release-artifacts
+ghr -prerelease -n "v${tag}" "${tag}" "${tmp_dir}"
+rm -rf "${tmp_dir}"
