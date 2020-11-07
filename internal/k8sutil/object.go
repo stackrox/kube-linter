3a8ecd7dfc70beaaae8f74391a46bf0ada46c6bd
Arghya Sadhu <arghya88@gmail.com>
2020-11-07 13:44:27 +0530

    fix imports for consistency

--- internal/k8sutil/object.go
+++ internal/k8sutil/object.go
@@ -1,12 +1,12 @@
 package k8sutil
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // Object is a combination of `runtime.Object` and `metav1.Object`.
 type Object interface {
 	runtime.Object
-	metav1.Object
+	metaV1.Object
 }
