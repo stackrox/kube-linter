3f1a7e594fe16bd042981b922a480861295bf5c9
Viswajith Venugopal <viswajithiii@gmail.com>
2020-11-12 16:03:20 -0800

    Add sanity test to ensure KubeLinter doesn't crash on any charts from the Helm chart repo

---
+++ e2etests/sanity_test.go
@@ -0,0 +1,44 @@
+// +build e2e
+
+package e2etests
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	kubeLinterBinEnv = "KUBE_LINTER_BIN"
+)
+
+func TestKubeLinterWithBuiltInChecksDoesntCrashOnHelmChartsRepo(t *testing.T) {
+	kubeLinterBin := os.Getenv(kubeLinterBinEnv)
+	require.NotEmpty(t, kubeLinterBin, "Please set %s", kubeLinterBinEnv)
+
+	_, err := os.Stat(kubeLinterBin)
+	require.NoError(t, err)
+
+	tmpDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, os.RemoveAll(tmpDir))
+	}()
+
+	chartsDir := filepath.Join(tmpDir, "charts")
+
+	gitCloneOut, err := exec.Command("git", "clone", "git@github.com:helm/charts.git", chartsDir).CombinedOutput()
+	require.NoError(t, err, "Git clone failed. output: %s", string(gitCloneOut))
+
+	kubeLinterOut, err := exec.Command(kubeLinterBin, "lint", chartsDir).CombinedOutput()
+	// Something will fail for sure, so kube-linter will not return a success code.
+	require.Error(t, err)
+	exitErr, ok := err.(*exec.ExitError)
+	require.True(t, ok)
+	assert.Equal(t, 1, exitErr.ExitCode(), "unexpected exit code: %d; output from kube-linter: %v", exitErr.ExitCode(), string(kubeLinterOut))
+}
