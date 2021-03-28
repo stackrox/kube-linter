cb34f2cc447f359d65713a0ef75336b5f231bcb4
aiyengar2 <arvind.iyengar@suse.com>
2021-03-27 18:19:08 -0700

    Add CreateContextWithOptions (#144)
    This will allow consumers of this package to provide non-default schemes to deserialize from or other advanced options via the options.
    
    Signed-off-by: Arvind Iyengar <arvind.iyengar@rancher.com>

--- pkg/lintcontext/create_contexts.go
+++ pkg/lintcontext/create_contexts.go
@@ -9,11 +9,19 @@ import (
 	"github.com/pkg/errors"
 	"golang.stackrox.io/kube-linter/internal/set"
 	"helm.sh/helm/v3/pkg/chartutil"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 var (
 	knownYAMLExtensions = set.NewFrozenStringSet(".yaml", ".yml")
 )
+
+// Options represent values that can be provided to modify how objects are parsed to create lint contexts
+type Options struct {
+	// CustomDecoder allows users to supply a non-default decoder to parse k8s objects. This can be used
+	// to allow the linter to create contexts for k8s custom resources
+	CustomDecoder runtime.Decoder
+}
 
 // CreateContexts creates a context. Each context contains a set of files that should be linted
 // as a group.
@@ -21,7 +29,11 @@ var (
 // TODO: Figure out if it's useful to allow people to specify that files spanning different directories
 // should be treated as being in the same context.
 func CreateContexts(filesOrDirs ...string) ([]LintContext, error) {
+	return CreateContextsWithOptions(Options{}, filesOrDirs...)
+}
 
+// CreateContextsWithOptions creates a context with additional Options
+func CreateContextsWithOptions(options Options, filesOrDirs ...string) ([]LintContext, error) {
 	contextsByDir := make(map[string]*lintContextImpl)
 	for _, fileOrDir := range filesOrDirs {
 		// Stdin
@@ -29,7 +41,7 @@ func CreateContexts(filesOrDirs ...string) ([]LintContext, error) {
 			if _, alreadyExists := contextsByDir["-"]; alreadyExists {
 				continue
 			}
-			ctx := new()
+			ctx := newCtx(options)
 			if err := ctx.loadObjectsFromReader("<standard input>", os.Stdin); err != nil {
 				return nil, err
 			}
@@ -48,7 +60,7 @@ func CreateContexts(filesOrDirs ...string) ([]LintContext, error) {
 
 			if !info.IsDir() {
 				if strings.HasSuffix(strings.ToLower(currentPath), ".tgz") {
-					ctx := new()
+					ctx := newCtx(options)
 					if err := ctx.loadObjectsFromTgzHelmChart(currentPath); err != nil {
 						return err
 					}
@@ -62,7 +74,7 @@ func CreateContexts(filesOrDirs ...string) ([]LintContext, error) {
 				if knownYAMLExtensions.Contains(strings.ToLower(filepath.Ext(currentPath))) || fileOrDir == currentPath {
 					ctx := contextsByDir[dirName]
 					if ctx == nil {
-						ctx = new()
+						ctx = newCtx(options)
 						contextsByDir[dirName] = ctx
 					}
 					if err := ctx.loadObjectsFromYAMLFile(currentPath, info); err != nil {
@@ -76,7 +88,7 @@ func CreateContexts(filesOrDirs ...string) ([]LintContext, error) {
 				if _, alreadyExists := contextsByDir[currentPath]; alreadyExists {
 					return nil
 				}
-				ctx := new()
+				ctx := newCtx(options)
 				contextsByDir[currentPath] = ctx
 				if err := ctx.loadObjectsFromHelmChart(currentPath); err != nil {
 					return err
