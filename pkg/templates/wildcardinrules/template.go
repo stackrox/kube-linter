4581f6402d70627625582e786ccf6ca55cfb9c93
Laura Luan <luan@us.ibm.com>
2021-06-09 14:20:37 -0400

    add template wildcard-in-rules
    Signed-off-by: Laura Luan <luan@us.ibm.com>

---
+++ pkg/templates/wildcardinrules/template.go
@@ -0,0 +1,74 @@
+package wildcardinrules
+
+import (
+	"fmt"
+
+	"golang.stackrox.io/kube-linter/pkg/check"
+	"golang.stackrox.io/kube-linter/pkg/config"
+	"golang.stackrox.io/kube-linter/pkg/diagnostic"
+	"golang.stackrox.io/kube-linter/pkg/extract"
+	"golang.stackrox.io/kube-linter/pkg/lintcontext"
+	"golang.stackrox.io/kube-linter/pkg/objectkinds"
+	"golang.stackrox.io/kube-linter/pkg/templates"
+	"golang.stackrox.io/kube-linter/pkg/templates/wildcardinrules/internal/params"
+	rbacV1 "k8s.io/api/rbac/v1"
+)
+
+var (
+	roleGVK        = rbacV1.SchemeGroupVersion.WithKind(objectkinds.Role)
+	clusterRoleGVK = rbacV1.SchemeGroupVersion.WithKind(objectkinds.ClusterRole)
+)
+
+func init() {
+	templates.Register(check.Template{
+		HumanName:   "Wildcard Use in Role and ClusterRole Rules",
+		Key:         "wildcard-in-rules",
+		Description: "Flag Roles and ClusterRoles that use wildcard * in rules",
+		SupportedObjectKinds: config.ObjectKindsDesc{
+			ObjectKinds: []string{
+				objectkinds.Role,
+				objectkinds.ClusterRole},
+		},
+		Parameters:             params.ParamDescs,
+		ParseAndValidateParams: params.ParseAndValidate,
+		Instantiate: params.WrapInstantiateFunc(func(_ params.Params) (check.Func, error) {
+			return func(lintCtx lintcontext.LintContext, object lintcontext.Object) []diagnostic.Diagnostic {
+				gvk := extract.GVK(object.K8sObject)
+				if gvk == roleGVK {
+					role, ok := object.K8sObject.(*rbacV1.Role)
+					if !ok {
+						return nil
+					}
+					return findWildCard(role.Rules)
+				}
+
+				if gvk == clusterRoleGVK {
+					crole, ok := object.K8sObject.(*rbacV1.ClusterRole)
+					if !ok {
+						return nil
+					}
+					return findWildCard(crole.Rules)
+				}
+				return nil
+			}, nil
+		}),
+	})
+}
+
+// find wildcards used in rules
+func findWildCard(rules []rbacV1.PolicyRule) []diagnostic.Diagnostic {
+	results := []diagnostic.Diagnostic{}
+	for _, rule := range rules {
+		for _, item := range rule.Resources {
+			if item == "*" {
+				results = append(results, diagnostic.Diagnostic{Message: fmt.Sprintf("wildcard %q in resource specification", item)})
+			}
+		}
+		for _, item := range rule.Verbs {
+			if item == "*" {
+				results = append(results, diagnostic.Diagnostic{Message: fmt.Sprintf("wildcard %q in verb specification", item)})
+			}
+		}
+	}
+	return results
+}
