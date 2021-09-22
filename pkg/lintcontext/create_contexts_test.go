04e3aac97dbb3bc25f32a8756c8e1222d81becfe
Mikhail Sugakov <misha@stackrox.com>
2021-09-22 20:18:50 +0200

    Add tests to verify object paths for loaded helm charts
    Archive and renamed directory tests fail at this point. The latter is
    noted in the issue https://github.com/stackrox/kube-linter/issues/212
    
    Note that when testing with modified chart directory name, I
    temporarily rename the directory and then rename it back at the end
    of the test.
    I do this because I don't see a simple way to copy directory tree in
    Go without writing own implementation. :-(
    Hopefully test will not crash often leaving renamed directory, or
    developer will notice and not commit files if it happens to crash.

--- pkg/lintcontext/create_contexts_test.go
+++ pkg/lintcontext/create_contexts_test.go
@@ -1,23 +1,98 @@
 package lintcontext
 
 import (
+	"os"
+	"path"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-const chartTarball = "../../tests/testdata/mychart-0.1.0.tgz"
+const (
+	chartTarball    = "../../tests/testdata/mychart-0.1.0.tgz"
+	chartDirectory  = "../../tests/testdata/mychart"
+	renamedChartDir = "../../tests/testdata/my-renamed-chart"
+)
 
-func TestCreateContextsFromHelmArchive(t *testing.T) {
-	lintCtxs, err := CreateContexts(chartTarball)
-	assert.NoError(t, err)
+func TestCreateContextFromHelmArchive(t *testing.T) {
+	lintCtx := createSingleContext(t, chartTarball)
 
-	var atLeastOneObjectFound bool
-	for _, lintCtx := range lintCtxs {
-		if len(lintCtx.Objects()) > 0 {
-			atLeastOneObjectFound = true
-			break
-		}
+	checkObjectPaths(t, lintCtx.Objects(), path.Join(chartTarball, "mychart"))
+}
+
+func TestCreateContextFromHelmArchiveAbsolutePath(t *testing.T) {
+	absPath, err := filepath.Abs(chartTarball)
+	require.NoError(t, err)
+
+	lintCtx := createSingleContext(t, absPath)
+
+	checkObjectPaths(t, lintCtx.Objects(), path.Join(absPath, "mychart"))
+}
+
+func TestCreateContextFromHelmDirectory(t *testing.T) {
+	lintCtx := createSingleContext(t, chartDirectory)
+
+	checkObjectPaths(t, lintCtx.Objects(), chartDirectory)
+}
+
+func TestCreateContextFromHelmDirectoryAbsolutePath(t *testing.T) {
+	absPath, err := filepath.Abs(chartDirectory)
+	require.NoError(t, err)
+	lintCtx := createSingleContext(t, absPath)
+
+	checkObjectPaths(t, lintCtx.Objects(), absPath)
+}
+
+func TestCreateContextFromRenamedHelmDirectory(t *testing.T) {
+	require.NoError(t, os.Rename(chartDirectory, renamedChartDir))
+	defer func() {
+		assert.NoError(t, os.Rename(renamedChartDir, chartDirectory))
+	}()
+
+	lintCtx := createSingleContext(t, renamedChartDir)
+
+	checkObjectPaths(t, lintCtx.Objects(), renamedChartDir)
+}
+
+func TestCreateContextFromRenamedHelmDirectoryAbsolutePath(t *testing.T) {
+	require.NoError(t, os.Rename(chartDirectory, renamedChartDir))
+	defer func() {
+		assert.NoError(t, os.Rename(renamedChartDir, chartDirectory))
+	}()
+
+	absPath, err := filepath.Abs(renamedChartDir)
+	require.NoError(t, err)
+
+	lintCtx := createSingleContext(t, absPath)
+
+	checkObjectPaths(t, lintCtx.Objects(), absPath)
+}
+
+func createSingleContext(t *testing.T, path string) LintContext {
+	lintCtxs, err := CreateContexts(path)
+	require.NoError(t, err)
+	assert.Len(t, lintCtxs, 1, "expecting single lint context to be returned")
+
+	lintCtx := lintCtxs[0]
+
+	assert.NotEmpty(t, lintCtx.Objects(), "no valid objects found")
+	assert.Empty(t, lintCtx.InvalidObjects(), "no invalid objects expected")
+
+	return lintCtx
+}
+
+func checkObjectPaths(t *testing.T, objects []Object, expectedPrefix string) {
+	actualPaths := make([]string, 0, len(objects))
+	for _, obj := range objects {
+		actualPaths = append(actualPaths, obj.Metadata.FilePath)
 	}
-	assert.True(t, atLeastOneObjectFound, "no valid objects found")
+	expectedPaths := []string{
+		path.Join(expectedPrefix, "templates/deployment.yaml"),
+		path.Join(expectedPrefix, "templates/service.yaml"),
+		path.Join(expectedPrefix, "templates/serviceaccount.yaml"),
+		path.Join(expectedPrefix, "templates/tests/test-connection.yaml"),
+	}
+	assert.ElementsMatchf(t, expectedPaths, actualPaths, "expected and actual template paths don't match")
 }
