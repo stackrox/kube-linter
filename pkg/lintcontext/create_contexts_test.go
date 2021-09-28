7764f9ab5f22d1f72d66ddd960ceb9395aa8d954
Mikhail Sugakov <misha@stackrox.com>
2021-09-28 20:58:59 +0200

    Add tests for object paths
    for both `CreateContexts` and `CreateContextsFromHelmArchive`.
    The test verifies all permutations of:
    * tarball vs directory chart
    * renamed vs original path
    * absolute vs relative path
    * `CreateContexts` and `CreateContextsFromHelmArchive` functions.
    
    The code looks a bit verbose but I believe this is still better than
    attempting to lay down each case separately.

--- pkg/lintcontext/create_contexts_test.go
+++ pkg/lintcontext/create_contexts_test.go
@@ -1,34 +1,94 @@
 package lintcontext
 
 import (
+	"fmt"
 	"os"
+	"path"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-const chartTarball = "../../tests/testdata/mychart-0.1.0.tgz"
+const (
+	chartTarball    = "../../tests/testdata/mychart-0.1.0.tgz"
+	chartDirectory  = "../../tests/testdata/mychart"
+	renamedTarball  = "../../tests/testdata/my-renamed-chart-0.1.0.tgz"
+	renamedChartDir = "../../tests/testdata/my-renamed-chart"
+	mockPath        = "mock path"
+)
 
-func TestCreateContextsFromHelmArchive(t *testing.T) {
-	file, err := os.Open(chartTarball)
-	require.NoError(t, err)
+func TestCreateContextsObjectPaths(t *testing.T) {
+	bools := []bool{false, true}
 
-	defer func() {
-		require.NoError(t, file.Close())
-	}()
+	for _, useTarball := range bools {
+		for _, absolute := range bools {
+			for _, rename := range bools {
+				for _, useFromArchiveFunction := range bools {
+					// CreateContextsFromHelmArchive can only be used with tarballs
+					if useFromArchiveFunction && !useTarball {
+						continue
+					}
 
-	lintCtxs, err := CreateContextsFromHelmArchive("test", file)
-	require.NoError(t, err)
-
-	assert.NotNil(t, verifyAndGetContext(t, lintCtxs))
+					testName := fmt.Sprintf("tarball %t, absolute path %t, rename %t, use from archive function %t", useTarball, absolute, rename, useFromArchiveFunction)
+					t.Run(testName, func(t *testing.T) {
+						createContextsAndVerifyPaths(t, useTarball, absolute, rename, useFromArchiveFunction)
+					})
+				}
+			}
+		}
+	}
 }
 
-func TestCreateContexts_WithHelmArchive(t *testing.T) {
-	lintCtxs, err := CreateContexts(chartTarball)
+func createContextsAndVerifyPaths(t *testing.T, useTarball, useAbsolutePath, rename, useFromArchiveFunction bool) {
+	var err error
+
+	// Arrange
+	relativePath := map[bool]string{false: chartDirectory, true: chartTarball}[useTarball]
+	renamedPath := map[bool]string{false: renamedChartDir, true: renamedTarball}[useTarball]
+
+	testPath := relativePath
+
+	if rename {
+		testPath = renamedPath
+		require.NoError(t, os.Rename(relativePath, renamedPath))
+		defer func() {
+			assert.NoError(t, os.Rename(renamedPath, relativePath))
+		}()
+	}
+
+	if useAbsolutePath {
+		testPath, err = filepath.Abs(testPath)
+		require.NoError(t, err)
+	}
+
+	// Act. The code actually tests either of functions: CreateContextsFromHelmArchive and CreateContexts
+	var lintCtxs []LintContext
+	if useFromArchiveFunction {
+		var file *os.File
+		file, err = os.Open(testPath)
+		require.NoError(t, err)
+
+		defer func() {
+			require.NoError(t, file.Close())
+		}()
+
+		lintCtxs, err = CreateContextsFromHelmArchive(mockPath, file)
+	} else {
+		lintCtxs, err = CreateContexts(testPath)
+	}
 	require.NoError(t, err)
 
-	assert.NotNil(t, verifyAndGetContext(t, lintCtxs))
+	// Assert
+	expectedPath := testPath
+	if useTarball {
+		expectedPath = path.Join(expectedPath, "mychart")
+	}
+	if useFromArchiveFunction {
+		expectedPath = path.Join(mockPath, "mychart")
+	}
+	checkObjectPaths(t, verifyAndGetContext(t, lintCtxs).Objects(), expectedPath)
 }
 
 func verifyAndGetContext(t *testing.T, lintCtxs []LintContext) LintContext {
@@ -39,4 +99,18 @@ func verifyAndGetContext(t *testing.T, lintCtxs []LintContext) LintContext {
 	assert.Empty(t, lintCtx.InvalidObjects(), "no invalid objects expected")
 
 	return lintCtx
+}
+
+func checkObjectPaths(t *testing.T, objects []Object, expectedPrefix string) {
+	actualPaths := make([]string, 0, len(objects))
+	for _, obj := range objects {
+		actualPaths = append(actualPaths, obj.Metadata.FilePath)
+	}
+	expectedPaths := []string{
+		path.Join(expectedPrefix, "templates/deployment.yaml"),
+		path.Join(expectedPrefix, "templates/service.yaml"),
+		path.Join(expectedPrefix, "templates/serviceaccount.yaml"),
+		path.Join(expectedPrefix, "templates/tests/test-connection.yaml"),
+	}
+	assert.ElementsMatchf(t, expectedPaths, actualPaths, "expected and actual template paths don't match")
 }
