05b78cde722a39330763c6d962bb6f476fcc9a2a
msugakov <537715+msugakov@users.noreply.github.com>
2021-09-28 20:08:21 +0200

    Update kube-linter dependencies (#224)

--- pkg/lintcontext/parse_yaml.go
+++ pkg/lintcontext/parse_yaml.go
@@ -130,8 +130,15 @@ func (l *lintContextImpl) loadObjectsFromHelmChart(dir string) error {
 		// The first element of path will be the same as the last element of dir, because
 		// Helm duplicates it.
 		pathToTemplate := filepath.Join(filepath.Dir(dir), path)
+
+		// Skip NOTES.txt file that may be present among templates but is not a kubernetes resource.
+		if strings.HasSuffix(pathToTemplate, string(filepath.Separator)+chartutil.NotesName) {
+			continue
+		}
+
 		if err := l.loadObjectsFromReader(pathToTemplate, strings.NewReader(contents)); err != nil {
-			return errors.Wrapf(err, "loading objects from rendered helm chart %s/%s", dir, pathToTemplate)
+			loadErr := errors.Wrapf(err, "loading object %s from rendered helm chart %s", pathToTemplate, dir)
+			l.addInvalidObjects(InvalidObject{Metadata: ObjectMetadata{FilePath: pathToTemplate}, LoadErr: loadErr})
 		}
 	}
 	return nil
@@ -148,8 +155,15 @@ func (l *lintContextImpl) loadObjectsFromTgzHelmChart(tgzFile string) error {
 		// The first element of path will be the same as the last element of tgzFile, because
 		// Helm duplicates it.
 		pathToTemplate := filepath.Join(filepath.Dir(tgzFile), path)
+
+		// Skip NOTES.txt file that may be present among templates but is not a kubernetes resource.
+		if strings.HasSuffix(pathToTemplate, string(filepath.Separator)+chartutil.NotesName) {
+			continue
+		}
+
 		if err := l.loadObjectsFromReader(pathToTemplate, strings.NewReader(contents)); err != nil {
-			return errors.Wrapf(err, "loading objects from rendered helm chart %s/%s", tgzFile, pathToTemplate)
+			loadErr := errors.Wrapf(err, "loading object %s from rendered helm chart %s", pathToTemplate, tgzFile)
+			l.addInvalidObjects(InvalidObject{Metadata: ObjectMetadata{FilePath: pathToTemplate}, LoadErr: loadErr})
 		}
 	}
 	return nil
@@ -278,18 +292,24 @@ func (l *lintContextImpl) renderTgzHelmChartReader(fileName string, tgzReader io
 	return l.renderChart(fileName, chrt)
 }
 
-func (l *lintContextImpl) readObjectsFromTgzHelmChart(fileName string, tgzReader io.Reader) error {
+func (l *lintContextImpl) readObjectsFromTgzHelmChart(fileName string, tgzReader io.Reader) {
 	metadata := ObjectMetadata{FilePath: fileName}
 	renderedFiles, err := l.renderTgzHelmChartReader(fileName, tgzReader)
 	if err != nil {
 		l.invalidObjects = append(l.invalidObjects, InvalidObject{Metadata: metadata, LoadErr: err})
-		return nil
+		return
 	}
 	for path, contents := range renderedFiles {
 		pathToTemplate := filepath.Join(fileName, path)
+
+		// Skip NOTES.txt file that may be present among templates but is not a kubernetes resource.
+		if strings.HasSuffix(pathToTemplate, string(filepath.Separator)+chartutil.NotesName) {
+			continue
+		}
+
 		if err := l.loadObjectsFromReader(pathToTemplate, strings.NewReader(contents)); err != nil {
-			return errors.Wrapf(err, "loading objects from rendered helm chart %s", pathToTemplate)
+			loadErr := errors.Wrapf(err, "loading object %s from rendered helm chart %s", pathToTemplate, fileName)
+			l.addInvalidObjects(InvalidObject{Metadata: ObjectMetadata{FilePath: pathToTemplate}, LoadErr: loadErr})
 		}
 	}
-	return nil
 }
