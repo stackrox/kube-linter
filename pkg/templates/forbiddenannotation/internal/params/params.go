5ca9e6fc9ff71eb30be7dcca97d8062c61f62768
Tomasz Janiszewski <tomek@redhat.com>
2022-01-10 14:43:38 +0100

    label to annotation
    Co-authored-by: Viswajith Venugopal <viswajithiii@gmail.com>

--- pkg/templates/forbiddenannotation/internal/params/params.go
+++ pkg/templates/forbiddenannotation/internal/params/params.go
@@ -3,7 +3,7 @@ package params
 // Params represents the params accepted by this template.
 type Params struct {
 
-	// Key of the forbidden label.
+	// Key of the forbidden annotation.
 	// +required
 	Key string
 
