3a606c6c1000a81b01fe22a33a50e40502affe51
rumstead <rjumstead@gmail.com>
2022-04-11 11:52:05 -0400

    pr review

--- pkg/command/lint/command_test.go
+++ pkg/command/lint/command_test.go
@@ -1,8 +1,8 @@
 package lint
 
 import (
-	"path/filepath"
-	"runtime"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/spf13/cobra"
@@ -12,20 +12,23 @@ import (
 )
 
 func TestCommand_InvalidResources(t *testing.T) {
-	testDataPath := getTestDataDir()
+	path, _ := os.Getwd()
+	fmt.Println(path)
 	tests := []struct {
 		name    string
 		cmd     *cobra.Command
 		failure bool
 		output  string
 	}{
-		{name: "InvalidPodResource", cmd: createLintCommand(testDataPath+"invalid-pod-resources.yaml", "--fail-on-invalid-resource"), failure: true},
-		{name: "InvalidPVCResource", cmd: createLintCommand(testDataPath+"invalid-pvc-resources.yaml", "--fail-on-invalid-resource"), failure: true},
+		{name: "InvalidPodResource", cmd: createLintCommand("./testdata/invalid-pod-resources.yaml", "--fail-on-invalid-resource"), failure: true},
+		{name: "InvalidPVCResource", cmd: createLintCommand("./testdata/invalid-pvc-resources.yaml", "--fail-on-invalid-resource"), failure: true},
+		{name: "NonexistentFile", cmd: createLintCommand("./testdata/foo-bar.yaml", "--fail-on-invalid-resource"), failure: true},
+		{name: "ValidPod", cmd: createLintCommand("./testdata/valid-pod.yaml", "--fail-on-invalid-resource"), failure: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := tt.cmd.Execute()
-			if err == nil && tt.failure {
+			if err == nil && tt.failure || err != nil && !tt.failure {
 				t.Fail()
 			}
 		})
@@ -34,12 +37,7 @@ func TestCommand_InvalidResources(t *testing.T) {
 
 func createLintCommand(args ...string) *cobra.Command {
 	c := Command()
+	c.SilenceUsage = true
 	c.SetArgs(args)
 	return c
-}
-
-func getTestDataDir() string {
-	_, b, _, _ := runtime.Caller(0)
-	basepath := filepath.Dir(b)
-	return basepath + "/testdata/"
 }
