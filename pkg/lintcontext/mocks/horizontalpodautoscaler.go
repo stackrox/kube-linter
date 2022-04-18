eb16b1f81f36f0c78a041e4519a71df11bef3e1c
Magnus Runesson <M.Runesson@gmail.com>
2022-04-18 13:13:44 +0200

    Add support for all current autoscaler hpa versions
    This adds support for all current autoscaler versions (v1, v2, v2beta1,
    and v2beta2) for hpa and template to test minireplicas at least 3.
    
    Fix issue: #287

--- pkg/lintcontext/mocks/horizontalpodautoscaler.go
+++ pkg/lintcontext/mocks/horizontalpodautoscaler.go
@@ -1,30 +1,88 @@
 package mocks
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"golang.stackrox.io/kube-linter/pkg/objectkinds"
+	autoscalingV1 "k8s.io/api/autoscaling/v1"
+	autoscalingV2 "k8s.io/api/autoscaling/v2"
 	autoscalingV2Beta1 "k8s.io/api/autoscaling/v2beta1"
+	autoscalingV2Beta2 "k8s.io/api/autoscaling/v2beta2"
+
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // AddMockHorizontalPodAutoscaler adds a mock HorizontalPodAutoscaler to LintContext
-func (l *MockLintContext) AddMockHorizontalPodAutoscaler(t *testing.T, name string) {
+func (l *MockLintContext) AddMockHorizontalPodAutoscaler(t *testing.T, name, version string) {
 	require.NotEmpty(t, name)
-	l.objects[name] = &autoscalingV2Beta1.HorizontalPodAutoscaler{
-		TypeMeta: metaV1.TypeMeta{
-			Kind:       objectkinds.HorizontalPodAutoscaler,
-			APIVersion: objectkinds.GetHorizontalPodAutoscalerAPIVersion(),
-		},
-		ObjectMeta: metaV1.ObjectMeta{Name: name},
-		Spec:       autoscalingV2Beta1.HorizontalPodAutoscalerSpec{},
+	switch version {
+	case "v2beta1":
+		l.objects[name] = &autoscalingV2Beta1.HorizontalPodAutoscaler{
+			TypeMeta: metaV1.TypeMeta{
+				Kind:       objectkinds.HorizontalPodAutoscaler,
+				APIVersion: objectkinds.GetHorizontalPodAutoscalerAPIVersion(version),
+			},
+			ObjectMeta: metaV1.ObjectMeta{Name: name},
+			Spec:       autoscalingV2Beta1.HorizontalPodAutoscalerSpec{},
+		}
+	case "v2beta2":
+		l.objects[name] = &autoscalingV2Beta2.HorizontalPodAutoscaler{
+			TypeMeta: metaV1.TypeMeta{
+				Kind:       objectkinds.HorizontalPodAutoscaler,
+				APIVersion: objectkinds.GetHorizontalPodAutoscalerAPIVersion(version),
+			},
+			ObjectMeta: metaV1.ObjectMeta{Name: name},
+			Spec:       autoscalingV2Beta2.HorizontalPodAutoscalerSpec{},
+		}
+	case "v2":
+		l.objects[name] = &autoscalingV2.HorizontalPodAutoscaler{
+			TypeMeta: metaV1.TypeMeta{
+				Kind:       objectkinds.HorizontalPodAutoscaler,
+				APIVersion: objectkinds.GetHorizontalPodAutoscalerAPIVersion(version),
+			},
+			ObjectMeta: metaV1.ObjectMeta{Name: name},
+			Spec:       autoscalingV2.HorizontalPodAutoscalerSpec{},
+		}
+	case "v1":
+		l.objects[name] = &autoscalingV1.HorizontalPodAutoscaler{
+			TypeMeta: metaV1.TypeMeta{
+				Kind:       objectkinds.HorizontalPodAutoscaler,
+				APIVersion: objectkinds.GetHorizontalPodAutoscalerAPIVersion(version),
+			},
+			ObjectMeta: metaV1.ObjectMeta{Name: name},
+			Spec:       autoscalingV1.HorizontalPodAutoscalerSpec{},
+		}
+	default:
+		require.FailNow(t, fmt.Sprintf("Unknown autoscaling version %s", version))
 	}
 }
 
-//ModifyHorizontalPodAutoscaler modifies a given HorizontalPodAutoscaler in the context via the passed function.
-func (l *MockLintContext) ModifyHorizontalPodAutoscaler(t *testing.T, name string, f func(hpa *autoscalingV2Beta1.HorizontalPodAutoscaler)) {
+//ModifyHorizontalPodAutoscalerV2Beta1 modifies a given HorizontalPodAutoscaler in the context via the passed function.
+func (l *MockLintContext) ModifyHorizontalPodAutoscalerV2Beta1(t *testing.T, name string, f func(hpa *autoscalingV2Beta1.HorizontalPodAutoscaler)) {
 	r, ok := l.objects[name].(*autoscalingV2Beta1.HorizontalPodAutoscaler)
+	require.True(t, ok)
+	f(r)
+}
+
+//ModifyHorizontalPodAutoscalerV2Beta2 modifies a given HorizontalPodAutoscaler in the context via the passed function.
+func (l *MockLintContext) ModifyHorizontalPodAutoscalerV2Beta2(t *testing.T, name string, f func(hpa *autoscalingV2Beta2.HorizontalPodAutoscaler)) {
+	r, ok := l.objects[name].(*autoscalingV2Beta2.HorizontalPodAutoscaler)
+	require.True(t, ok)
+	f(r)
+}
+
+//ModifyHorizontalPodAutoscalerV2 modifies a given HorizontalPodAutoscaler in the context via the passed function.
+func (l *MockLintContext) ModifyHorizontalPodAutoscalerV2(t *testing.T, name string, f func(hpa *autoscalingV2.HorizontalPodAutoscaler)) {
+	r, ok := l.objects[name].(*autoscalingV2.HorizontalPodAutoscaler)
+	require.True(t, ok)
+	f(r)
+}
+
+//ModifyHorizontalPodAutoscalerV1 modifies a given HorizontalPodAutoscaler in the context via the passed function.
+func (l *MockLintContext) ModifyHorizontalPodAutoscalerV1(t *testing.T, name string, f func(hpa *autoscalingV1.HorizontalPodAutoscaler)) {
+	r, ok := l.objects[name].(*autoscalingV1.HorizontalPodAutoscaler)
 	require.True(t, ok)
 	f(r)
 }
--- pkg/templates/hpareplicas/template_test.go
+++ pkg/templates/hpareplicas/template_test.go
@@ -1,15 +1,23 @@
 package hpareplicas
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/suite"
 	"golang.stackrox.io/kube-linter/pkg/diagnostic"
 	"golang.stackrox.io/kube-linter/pkg/lintcontext/mocks"
 	"golang.stackrox.io/kube-linter/pkg/templates"
 	"golang.stackrox.io/kube-linter/pkg/templates/hpareplicas/internal/params"
+	autoscalingV1 "k8s.io/api/autoscaling/v1"
+	autoscalingV2 "k8s.io/api/autoscaling/v2"
 	autoscalingV2Beta1 "k8s.io/api/autoscaling/v2beta1"
+	autoscalingV2Beta2 "k8s.io/api/autoscaling/v2beta2"
 )
+
+var autoscalingVersions = [4]string{"v2beta1", "v2beta2", "v2", "v1"}
 
 func TestHPAReplicas(t *testing.T) {
 	suite.Run(t, new(HPAReplicaTestSuite))
@@ -26,11 +34,28 @@ func (s *HPAReplicaTestSuite) SetupTest() {
 	s.ctx = mocks.NewMockContext()
 }
 
-func (s *HPAReplicaTestSuite) addHPAWithReplicas(name string, replicas int32) {
-	s.ctx.AddMockHorizontalPodAutoscaler(s.T(), name)
-	s.ctx.ModifyHorizontalPodAutoscaler(s.T(), name, func(hpa *autoscalingV2Beta1.HorizontalPodAutoscaler) {
-		hpa.Spec.MinReplicas = &replicas
-	})
+func (s *HPAReplicaTestSuite) addHPAWithReplicas(name string, replicas int32, version string) {
+	s.ctx.AddMockHorizontalPodAutoscaler(s.T(), name, version)
+	switch version {
+	case "v2beta1":
+		s.ctx.ModifyHorizontalPodAutoscalerV2Beta1(s.T(), name, func(hpa *autoscalingV2Beta1.HorizontalPodAutoscaler) {
+			hpa.Spec.MinReplicas = &replicas
+		})
+	case "v2beta2":
+		s.ctx.ModifyHorizontalPodAutoscalerV2Beta2(s.T(), name, func(hpa *autoscalingV2Beta2.HorizontalPodAutoscaler) {
+			hpa.Spec.MinReplicas = &replicas
+		})
+	case "v2":
+		s.ctx.ModifyHorizontalPodAutoscalerV2(s.T(), name, func(hpa *autoscalingV2.HorizontalPodAutoscaler) {
+			hpa.Spec.MinReplicas = &replicas
+		})
+	case "v1":
+		s.ctx.ModifyHorizontalPodAutoscalerV1(s.T(), name, func(hpa *autoscalingV1.HorizontalPodAutoscaler) {
+			hpa.Spec.MinReplicas = &replicas
+		})
+	default:
+		require.FailNow(s.T(), fmt.Sprintf("Unknown autoscaling version %s", version))
+	}
 }
 
 func (s *HPAReplicaTestSuite) TestTooFewReplicas() {
@@ -39,42 +64,47 @@ func (s *HPAReplicaTestSuite) TestTooFewReplicas() {
 		twoReplicasHPAName        = "hpa-two-replicas"
 	)
 
-	s.ctx.AddMockHorizontalPodAutoscaler(s.T(), noExplicitReplicasHPAName)
-	s.addHPAWithReplicas(twoReplicasHPAName, 2)
+	for _, version := range autoscalingVersions {
+		s.ctx.AddMockHorizontalPodAutoscaler(s.T(), noExplicitReplicasHPAName, version)
+		s.addHPAWithReplicas(twoReplicasHPAName, 2, version)
 
-	s.Validate(s.ctx, []templates.TestCase{
-		{
-			Param: params.Params{
-				MinReplicas: 3,
-			},
-			Diagnostics: map[string][]diagnostic.Diagnostic{
-				noExplicitReplicasHPAName: {
-					{Message: "object has 1 replica but minimum required replicas is 3"},
+		s.Validate(s.ctx, []templates.TestCase{
+			{
+				Param: params.Params{
+					MinReplicas: 3,
 				},
-				twoReplicasHPAName: {
-					{Message: "object has 2 replicas but minimum required replicas is 3"},
+				Diagnostics: map[string][]diagnostic.Diagnostic{
+					noExplicitReplicasHPAName: {
+						{Message: "object has 1 replica but minimum required replicas is 3"},
+					},
+					twoReplicasHPAName: {
+						{Message: "object has 2 replicas but minimum required replicas is 3"},
+					},
 				},
+				ExpectInstantiationError: false,
 			},
-			ExpectInstantiationError: false,
-		},
-	})
+		})
+	}
 }
 
 func (s *HPAReplicaTestSuite) TestAcceptableReplicas() {
 	const (
 		acceptableReplicasHPAName = "hpa-acceptable-replicas"
 	)
-	s.addHPAWithReplicas(acceptableReplicasHPAName, 3)
 
-	s.Validate(s.ctx, []templates.TestCase{
-		{
-			Param: params.Params{
-				MinReplicas: 3,
+	for _, version := range autoscalingVersions {
+		s.addHPAWithReplicas(acceptableReplicasHPAName, 3, version)
+
+		s.Validate(s.ctx, []templates.TestCase{
+			{
+				Param: params.Params{
+					MinReplicas: 3,
+				},
+				Diagnostics: map[string][]diagnostic.Diagnostic{
+					acceptableReplicasHPAName: nil,
+				},
+				ExpectInstantiationError: false,
 			},
-			Diagnostics: map[string][]diagnostic.Diagnostic{
-				acceptableReplicasHPAName: nil,
-			},
-			ExpectInstantiationError: false,
-		},
-	})
+		})
+	}
 }
