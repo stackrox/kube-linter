95449f0c4de3edbe4888a7758ffa05a5ac7197f1
Charlie OConor <coconor@ocrolus.com>
2022-08-09 02:00:20 -0500

    Support all hpa types in dangling-hpa

--- pkg/templates/danglinghpa/template.go
+++ pkg/templates/danglinghpa/template.go
@@ -11,12 +11,56 @@ import (
 	"golang.stackrox.io/kube-linter/pkg/objectkinds"
 	"golang.stackrox.io/kube-linter/pkg/templates"
 	"golang.stackrox.io/kube-linter/pkg/templates/danglinghpa/internal/params"
+	autoscalingV1 "k8s.io/api/autoscaling/v1"
+	autoscalingV2 "k8s.io/api/autoscaling/v2"
 	autoscalingV2Beta1 "k8s.io/api/autoscaling/v2beta1"
+	autoscalingV2Beta2 "k8s.io/api/autoscaling/v2beta2"
 )
 
 const (
 	templateKey = "dangling-horizontalpodautoscaler"
 )
+
+type objectReference struct {
+	Kind       string
+	Name       string
+	APIVersion string
+}
+
+func getTargetFromHpa(obj lintcontext.Object) (*objectReference, bool) {
+	switch hpa := obj.K8sObject.(type) {
+	case *autoscalingV1.HorizontalPodAutoscaler:
+		target := hpa.Spec.ScaleTargetRef
+		return &objectReference{
+			Kind:       target.Kind,
+			Name:       target.Name,
+			APIVersion: target.APIVersion,
+		}, true
+	case *autoscalingV2Beta1.HorizontalPodAutoscaler:
+		target := hpa.Spec.ScaleTargetRef
+		return &objectReference{
+			Kind:       target.Kind,
+			Name:       target.Name,
+			APIVersion: target.APIVersion,
+		}, true
+	case *autoscalingV2Beta2.HorizontalPodAutoscaler:
+		target := hpa.Spec.ScaleTargetRef
+		return &objectReference{
+			Kind:       target.Kind,
+			Name:       target.Name,
+			APIVersion: target.APIVersion,
+		}, true
+	case *autoscalingV2.HorizontalPodAutoscaler:
+		target := hpa.Spec.ScaleTargetRef
+		return &objectReference{
+			Kind:       target.Kind,
+			Name:       target.Name,
+			APIVersion: target.APIVersion,
+		}, true
+	default:
+		return nil, false
+	}
+}
 
 func init() {
 	templates.Register(check.Template{
@@ -30,7 +74,7 @@ func init() {
 		ParseAndValidateParams: params.ParseAndValidate,
 		Instantiate: params.WrapInstantiateFunc(func(p params.Params) (check.Func, error) {
 			return func(lintCtx lintcontext.LintContext, object lintcontext.Object) []diagnostic.Diagnostic {
-				hpa, found := object.K8sObject.(*autoscalingV2Beta1.HorizontalPodAutoscaler)
+				target, found := getTargetFromHpa(object)
 				if !found {
 					return nil
 				}
@@ -39,7 +83,6 @@ func init() {
 					if !hasPods {
 						continue
 					}
-					target := hpa.Spec.ScaleTargetRef
 					k8sObj := obj.K8sObject
 					gvk := k8sObj.GetObjectKind().GroupVersionKind()
 					if target.Name == obj.GetK8sObjectName().Name && target.Kind == gvk.Kind && target.APIVersion == gvk.Group+"/"+gvk.Version {
@@ -47,7 +90,7 @@ func init() {
 						return nil
 					}
 				}
-				return []diagnostic.Diagnostic{{Message: fmt.Sprintf("no resources found matching HorizontalPodAutoscaler scaleTargetRef (%v)", hpa.Spec.ScaleTargetRef)}}
+				return []diagnostic.Diagnostic{{Message: fmt.Sprintf("no resources found matching HorizontalPodAutoscaler scaleTargetRef (%v)", *target)}}
 			}, nil
 		}),
 	})
