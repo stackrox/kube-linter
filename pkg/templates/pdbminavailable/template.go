b95e442ec69ffe1fecefc79244d5a78df8cd8b56
Stephan Heßelmann <shesselm@redhat.com>
2025-01-07 02:28:46 +0100

    chore: bump golangci-lint to v1.63.4

--- pkg/templates/pdbminavailable/template.go
+++ pkg/templates/pdbminavailable/template.go
@@ -114,6 +114,7 @@ func minAvailableCheck(lintCtx lintcontext.LintContext, object lintcontext.Objec
 			// Calulate the actual value of the MinAvailable with respect to the Replica count if a percentage is set
 			pdbMinAvailable = int(math.Ceil(float64(replicas) * (float64(value) / float64(100))))
 		}
+		//nolint:gosec // Integer conversion should be safe here since the kube api uses int32.
 		if replicas <= int32(pdbMinAvailable) {
 			results = append(results, diagnostic.Diagnostic{
 				Message: fmt.Sprintf("The current number of replicas for deployment %s is equal to or lower than the minimum number of replicas specified by its PDB.", dl.GetName()),
