edb3b49a8f3d15b665b7a46808d9415e342d7b69
Yann Brillouet <ybrillou@redhat.com>
2025-07-10 13:59:25 +0200

    gofmt fix

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -10,18 +10,17 @@ func Wrap(err error, msg string) error {
 }
 
 func Wrapf(err error, format string, args ...any) error {
-	return fmt.Errorf(format + ": %w)", append(args, err)...)
+	return fmt.Errorf(format+": %w)", append(args, err)...)
 }
 
 func Errorf(format string, args ...any) error {
-	return fmt.Errorf(format, args)
+	return fmt.Errorf(format, args...)
 }
 
 func New(msg string) error {
 	return errors.New(msg)
 }
 
-func Is(err error, target error) bool {
+func Is(err, target error) bool {
 	return errors.Is(err, target)
 }
-
