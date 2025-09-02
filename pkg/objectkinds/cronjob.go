090906ff06a7626a6acffcf3a1ead1322cdc4ef0
Tomasz Janiszewski <tomek@redhat.com>
2025-09-02 13:01:29 +0200

    move kinds
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- pkg/objectkinds/cronjob.go
+++ pkg/objectkinds/cronjob.go
@@ -1,12 +1,18 @@
 package objectkinds
 
 import (
+	batchV1 "k8s.io/api/batch/v1"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 const (
 	// CronJob represents Kubernetes CronJob objects.
 	CronJob = "CronJob"
+)
+
+var (
+	cronJobGVK = batchV1.SchemeGroupVersion.WithKind("CronJob")
 )
 
 func init() {
--- pkg/objectkinds/job.go
+++ pkg/objectkinds/job.go
@@ -1,12 +1,18 @@
 package objectkinds
 
 import (
+	batchV1 "k8s.io/api/batch/v1"
+
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 const (
 	// Job represents Kubernetes Job objects.
 	Job = "Job"
+)
+
+var (
+	jobGVK = batchV1.SchemeGroupVersion.WithKind("Job")
 )
 
 func init() {
