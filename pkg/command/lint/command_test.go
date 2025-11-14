b14347598458da5f81441a30bf5d7964299ea737
Tomasz Janiszewski <tomek@redhat.com>
2025-11-14 14:09:08 +0100

    remove duplicated tests
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- pkg/command/lint/command_test.go
+++ pkg/command/lint/command_test.go
@@ -16,7 +16,6 @@ func TestCommand_InvalidResources(t *testing.T) {
 		failure bool
 		output  string
 	}{
-		{name: "InvalidPodResource", cmd: createLintCommand("./testdata/invalid-pod-resources.yaml", "--fail-on-invalid-resource"), failure: true},
 		{name: "InvalidYAML", cmd: createLintCommand("./testdata/invalid.yaml", "--fail-on-invalid-resource"), failure: true},
 		{name: "NonexistentFile", cmd: createLintCommand("./testdata/foo-bar.yaml", "--fail-on-invalid-resource"), failure: true},
 		{name: "ValidPod", cmd: createLintCommand("./testdata/valid-pod.yaml", "--fail-on-invalid-resource"), failure: false},
